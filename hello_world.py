e95b5afed6f08b1a001cb41149928aea5526170a
andrewglory32@gmail.com <andrewglory32@gmail.com>
2019-02-07 05:07:34 -0800

    first commit on 100 days of code

---
+++ hello_world.py
@@ -0,0 +1,8 @@
+print("Hello Python world!")
+
+message = "Hello Python world!"
+print(message)
+
+#changing the values of the variables
+message = "hello python world, this is my first python code"
+print(message)
